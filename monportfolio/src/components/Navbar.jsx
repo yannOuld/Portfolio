3a66810959e5186c7b78d9d9720755fe70b885ad
Yannis ould abderrahmane <yannis.ouldab@outlook.fr>
2023-03-11 02:25:29 +0100

    Navigation progress

--- monportfolio/src/components/Navbar.jsx
+++ monportfolio/src/components/Navbar.jsx
@@ -1,9 +1,51 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
+import { styles } from "../style.js";
+import { navLinks } from "../constants";
+import { logo, menu, close } from "../assets";
 
 const Navbar = () => {
+  const [active, setActive] = useState("");
   return (
-    <div>Navbar</div>
-  )
-}
+    <nav
+      className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}
+    >
+      <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
+        <Link
+          to="/"
+          className="flex items-center gap-2"
+          onClick={() => {
+            setActive("");
+            window.scrollTo(0, 0);
+          }}
+        >
+          <img
+            src={logo}
+            alt="logo"
+            className="w-[100px] flex h-[100px] object-cover rounded-full"
+          />
+          <p className="text-white text-[18px] font-bold cursor-pointer">
+            Yannis Ould
+            <span className="sm:block hidden">
+              Developpeur Javascript
+            </span>
+          </p>
+        </Link>
+        <ul className="list-none hidden sm:flex flex-row gap-10">
+          {navLinks.map((link) => (
+            <li
+              key={link.id}
+              className={`${active === link.title ? "text-white" : "text-secondary"
+                } hover:text-white text-[18px] font-medium cursor-pointer`}
+            >
+              <a href={`#${link.id}`}> {link.title} </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
